8eb2aa472c48c5d7167b1c17cf369a18fe412e49
Orleydovsky <orleydovsky@gmail.com>
2022-07-28 15:56:17 -0500

    feat: create FullPageLogo component

---
+++ src/pages/FullPageLogo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import icon from '../assets/icon.svg'
+export default function FullPageLogo () {
+  return (
+    <div className='grid h-screen place-items-center'>
+      <img width='60px' src={icon}/>
+    </div>
+  )
+}
