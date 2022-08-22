020de99014a2733b458e3449dcb5963c770e6387
Orleydovsky <orleydovsky@gmail.com>
2022-08-22 14:25:24 -0500

    fix: provide postId as key for rendered post components

--- src/components/profile/posts.tsx
+++ src/components/profile/posts.tsx
@@ -4,9 +4,9 @@ export default function ProfilePosts ({ posts }: any) {
     <article className='grid grid-cols-3 gap-1 container mx-auto max-w-screen-md mt-5'>
       {
         posts?.map((post: any) => {
-          const { contentSrc } = post
+          const { contentSrc, postId } = post
           return (
-            <div key={1} className='bg-black w-full aspect-square'>
+            <div key={postId} className='bg-black w-full aspect-square'>
               <img src={contentSrc} className='w-full h-full object-cover'/>
             </div>
           )
